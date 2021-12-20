77976e96f978c40c7209ab9fc6c3b64a3819216e
MartinMilan <martin.milan3@gmail.com>
2021-12-20 14:35:29 -0300

    Se crea proyecto

---
+++ desafiogl/src/main/java/com/mm/desafiogl/DesafioglApplication.java
@@ -0,0 +1,13 @@
+package com.mm.desafiogl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioglApplication.class, args);
+	}
+
+}
---
+++ desafiogl/src/test/java/com/mm/desafiogl/DesafioglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.desafiogl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
